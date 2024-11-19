49684dfad21d28209ce96b790c7c56e52b0a31f6
Marco Antonio <cardona.marco@iexe.edu.mx>
2024-11-18 21:35:14 -0600

    Añadimos jquery

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,27 +1,29 @@
-    <script src="<?php echo base_url(); ?>assets/plugins/jquery/dist/jquery.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/jquery-ui/jquery-ui.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/tether/dist/js/tether.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Required Fremwork -->
-    <script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/tether/dist/js/tether.min.js"></script>
 
-    <!-- waves effects.js -->
-    <script src="<?php echo base_url(); ?>assets/plugins/Waves/waves.min.js"></script>
+<!-- Required Fremwork -->
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Scrollbar JS-->
-    <script src="<?php echo base_url(); ?>assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/jquery.nicescroll/jquery.nicescroll.min.js"></script>
+<!-- waves effects.js -->
+<script src="<?php echo base_url(); ?>assets/plugins/Waves/waves.min.js"></script>
 
-    <!--classic JS-->
-    <script src="<?php echo base_url(); ?>assets/plugins/classie/classie.js"></script>
+<!-- Scrollbar JS-->
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jquery.nicescroll/jquery.nicescroll.min.js"></script>
 
-    <!-- notification -->
-    <script src="<?php echo base_url(); ?>assets/plugins/notification/js/bootstrap-growl.min.js"></script>
+<!--classic JS-->
+<script src="<?php echo base_url(); ?>assets/plugins/classie/classie.js"></script>
 
-    <!-- custom js -->
-    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/main.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>assets/pages/elements.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/menu.min.js"></script>
+<!-- notification -->
+<script src="<?php echo base_url(); ?>assets/plugins/notification/js/bootstrap-growl.min.js"></script>
 
-    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/sistema/<?php echo $js; ?>"></script>
+<!-- custom js -->
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/main.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/pages/elements.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/menu.min.js"></script>
+
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/sistema/<?php echo $js; ?>"></script>
